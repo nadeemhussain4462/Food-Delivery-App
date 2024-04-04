9694444a46077f4c216c4438b7c0f69c618b4d6a
Nadeem Hussain <81299514+nadeemhussain4462@users.noreply.github.com>
2024-04-05 00:04:29 +0500

    food site navbar update

---
+++ src/components/Notification.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Notification = () => {
+  return <div className="h-12 bg-red-500 text-white px-4 flex items-center text-center justify-center text-sm md:text-base cursor-pointer">Free delivery for all orders over $50. Order your food now!</div>;
+};
+
+export default Notification;
