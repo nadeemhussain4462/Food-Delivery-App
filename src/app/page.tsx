1b9ec338d18595c09e54a77a960ecd2b0f8d7805
Nadeem Hussain <81299514+nadeemhussain4462@users.noreply.github.com>
2024-04-05 23:19:18 +0500

    slider update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import Footer from "@/components/Footer";
+import Offer from "@/components/Offer";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Food Delivery App</h1>
-    </div>
-  )
+    <main>
+      <Slider />
+      <Footer />
+      <Offer />
+    </main>
+  );
 }
